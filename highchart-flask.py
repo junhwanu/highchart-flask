0c81589efca24a4bd90b74f5b8b13a221899f5d9
junhwanu <junhwanu@gmail.com>
2019-04-13 18:45:09 +0900

    + dummy.json / highchart-flask.py   - dummy.json 매번 새로 생성하는게 아니라 File이 없으면 생성하도록 변경
    / simulate.html - bootstrap 추가
      - Next, Prev 기능 구현
        - 버그가 있는데... dummy data의 367 ~ 368(금~일요일, 일요일 데이터가 왜있지?)로 시간이 확 넘어가는 부분에 series[idx].data list에 공백이 생김. 밑에 Navigator series 기준으로 생성되고 없는 시간은 비워두는 것 같음... error는 발생하지만 기능적으로는 이상이 없어 보임.

--- highchart-flask.py
+++ highchart-flask.py
@@ -17,35 +17,47 @@ def index():
 
 @app.route('/dummy.json')
 def dummy():
-    dbm = DBManager()
-    d = dbm.request_min_candle('CLJ19', '15', start_date='2016-12-06', end_date='2020-12-15')
-
     dummy = []
-    subject_code = 'CLJ19'
-    chart_type = 'min_15'
-    idx = 0
-    ADD = 'added'
-    MODIFY = 'modified'
-    REMOVE = 'removed'
-    for candle in d:
-        info = {}
-        info[chart_type] = {
-            ADD: {},
-            MODIFY: {},
-            REMOVE: {}
-        }
 
-        date = int(time.mktime(datetime.datetime.strptime(candle['date'], "%Y-%m-%d %H:%M:%S").timetuple()) * 1000)
-        # print(date)
-        info[chart_type][ADD]['candlestick'] = {
-            'name': subject_code,
-            'data': [idx, date, candle['open'], candle['high'], candle['low'], candle['close'], candle['volume']]
-        }
+    try:
+        f = open('dummy.json', 'r')
+        dummy = json.load(f)
 
-        dummy.append(info)
-        idx += 1
+    except Exception as err:
+        print(err)
 
-    pprint(dummy)
+        dbm = DBManager()
+        d = dbm.request_min_candle('CLJ19', '15', start_date='2016-12-06', end_date='2020-12-15')
+
+        subject_code = 'CLJ19'
+        chart_type = 'min_15'
+        idx = 0
+        ADD = 'added'
+        MODIFY = 'modified'
+        REMOVE = 'removed'
+        for candle in d:
+            info = {}
+            info[chart_type] = {
+                ADD: {},
+                MODIFY: {},
+                REMOVE: {}
+            }
+
+            date = int(time.mktime(datetime.datetime.strptime(candle['date'], "%Y-%m-%d %H:%M:%S").timetuple()) * 1000)
+            # print(date)
+            info[chart_type][ADD]['candlestick'] = {
+                'name': subject_code,
+                'data': [idx, date, candle['open'], candle['high'], candle['low'], candle['close'], candle['volume']]
+            }
+
+            dummy.append(info)
+            idx += 1
+
+        pprint(dummy)
+
+        with open('dummy.json', 'w') as f:
+            json.dump(dummy, f)
+
     return json.dumps(dummy)
 
 if __name__ == '__main__':
